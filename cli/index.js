f5b775231edac4aceefd61aa0bdb4a532d6f379f
Ashish Kurian Thomas <ashish@mindwaveventures.com>
2022-12-28 14:57:04 +0530

    refactor reusable code for server and cli

--- cli/index.js
+++ cli/index.js
@@ -1,44 +1,13 @@
 require("dotenv").config();
-const db = require("./db");
 
-function printHelp() {
-  console.log(`
-usage:
-node cli fetch-all
-node cli fetch id
-node cli insert "book name"
-node cli update id "book name"
-node cli delete id
-  `);
-}
-
-function insertNovel(title) {
-  const now = `${new Date().toISOString()}`;
-  const insertText =
-    "INSERT INTO novels (title, createdat, updatedat) VALUES ($1, $2, $3) RETURNING *";
-  return db.query(insertText, [title, now, now]);
-}
-
-function fetchAllNovels() {
-  return db.query(`SELECT id, title FROM novels`);
-}
-
-function getNovel(id) {
-  const query = `SELECT id, title, createdat, updatedat FROM novels WHERE id=$1`;
-  return db.query(query, [id]);
-}
-
-function updateNovel(id, title) {
-  const now = `${new Date().toISOString()}`;
-  const updateText =
-    "UPDATE novels SET title=$2, updatedat=$3 WHERE id=$1 RETURNING *";
-  return db.query(updateText, [id, title, now]);
-}
-
-function deleteNovel(id) {
-  const query = `DELETE FROM novels WHERE id=$1`;
-  return db.query(query, [id]);
-}
+const {
+  printHelp,
+  insertNovel,
+  fetchAllNovels,
+  getNovel,
+  updateNovel,
+  deleteNovel,
+} = require("../utils");
 
 async function main() {
   // node cli fetch-all
